97938cd347108a5a0fb081ae3d4bbdc74ec4073d
Rafael Candido <rafaelhenriquesilva494@gmail.com>
2024-06-26 07:35:52 -0300

    create a template file to test entity class

---
+++ src/infra/template/entity-test-template.ts
@@ -0,0 +1,9 @@
+/**
+ * MOCK PARA CRIAR O OBJETO NA CLASSE DE TEST
+  
+    number: faker.number.float()
+    string: faker.string.sample()
+    buffer: new Buffer(faker.string.sample())
+    any: {}
+ * 
+ */
