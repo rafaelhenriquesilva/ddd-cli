e8d8e33efcfbbbad36cf3c4a58177c07ff6b00a1
Rafael Candido <rafaelhenriquesilva494@gmail.com>
2024-06-29 17:48:47 -0300

    create other table to test

--- sql/tables.sql
+++ sql/tables.sql
@@ -24,3 +24,22 @@ CREATE TRIGGER update_developer_updated_at
 BEFORE UPDATE ON developer
 FOR EACH ROW
 EXECUTE PROCEDURE update_updated_at_column();
+
+
+-- Create the language_developer table
+CREATE TABLE IF NOT EXISTS language_developer (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    developer_id UUID NOT NULL,
+    language TEXT NOT NULL,
+    experience_time INT NOT NULL, -- Time in months or years, as preferred
+    level TEXT NOT NULL, -- e.g., 'Beginner', 'Intermediate', 'Advanced'
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (developer_id) REFERENCES developer(id) ON DELETE CASCADE
+);
+
+-- Create the trigger to update the updated_at field in the language_developer table
+CREATE TRIGGER update_language_developer_updated_at
+BEFORE UPDATE ON language_developer
+FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at_column();
